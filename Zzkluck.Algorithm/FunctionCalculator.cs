14ae22a8ad5aee655c10764029da48c7a29f7dba
zzkluckPC <zzkluck@outlook.com>
2017-02-17 12:24:32 +0800

    添加了一个用于数学计算的模块，还需要完善

---
+++ Zzkluck.Algorithm/FunctionCalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zzkluck
+{
+	public static class FunctionCalculator
+	{
+		public delegate double OneParamFunction(double x);
+		public delegate double TwoParamFunction(double x,double y);
+		public delegate double ThreeParamFunction(double x,double y,double z);
+
+		public static List<FCResult> calculate(double min,double max,double stepLength,ThreeParamFunction f)
+		{
+			List<FCResult> results = new List<FCResult>();
+			for (double x = min;  x< max; x+=stepLength)
+			{
+				for(double y = min; y < 1-x; y += stepLength)
+				{
+					double z = 1 - x - y;
+					results.Add(new FCResult(f(x, y, z), x, y, z));
+				}
+			}
+			return results;
+
+		} 
+
+	}
+
+	public class FCResult
+	{
+		public double result;
+		public double x, y, z;
+		public FCResult(double result,double x,double y,double z)
+		{
+			this.result = result;
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+	}
+
+}
---
+++ Zzkluck.AlgorithmTests/FunctionCalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zzkluck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zzkluck.Tests
+{
+	[TestClass()]
+	public class FunctionCalculatorTests
+	{
+		[TestMethod()]
+		public void calculateTest()
+		{
+			List<FCResult> result=FunctionCalculator.calculate(0, 1, 0.001, f);
+			FCResult r = result[0];
+			foreach (FCResult item in result)
+			{
+				if (item.result > r.result)
+					r = item;
+			}
+
+			double aaa = r.result;
+		}
+
+		double f(double x,double y,double z)
+		{
+			return 0.225 * x + 0.263 * y + 0.172 * z - 0.348 * x * y - 0.29 * x * z - 0.314 * y * z - 0.087 * x * y * z;
+		}
+	}
+}
